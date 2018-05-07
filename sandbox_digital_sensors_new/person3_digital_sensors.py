16a4622ff1be070a64e383fce61229a96bed61eb
zouz-sean <zouz@rose-hulman.edu>
2018-05-07 09:48:38 -0400

    robot

--- sandbox_digital_sensors_new/person3_digital_sensors.py
+++ sandbox_digital_sensors_new/person3_digital_sensors.py
@@ -7,11 +7,11 @@ Person 1: ev3.TouchSensor
 Person 2: ev3.Button
 Person 3: ev3.RemoteControl
 
-Authors: David Fisher, David Mutchler and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher, David Mutchler and Zhengxiao Zou.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # -----------------------------------------------------------------------------
-# TODO: 2.  WITH YOUR INSTRUCTOR, discuss the "big picture" of this project,
+# DONE: 2.  WITH YOUR INSTRUCTOR, discuss the "big picture" of this project,
 #           as described in the   _README_FIRST.txt   file.
 #
 # When your   ** ENTIRE TEAM ** understands that:
@@ -33,7 +33,7 @@ def main():
     """ Calls the   TEST   functions in this module. """
     # Uncomment these tests as you proceed through this module.
 
-    # run_test_buttons_on_ir_beacon()
+    run_test_buttons_on_ir_beacon()
     # run_test_wait_for_press_on_ir_beacon_button()
     # run_test_make_sounds()
 
@@ -120,6 +120,13 @@ def print_state_of_blue_up_button_on_ir_beacon(n, seconds_per_print):
     # TODO: 3.  Implement and test this function.
     #           Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    RC = ev3.RemoteControl(1)
+    assert RC
+
+    for k in range (n):
+        time.sleep(seconds_per_print)
+        print(RC.blue_up)
+
 
 
 def run_test_wait_for_press_on_ir_beacon_button():
@@ -186,6 +193,9 @@ def wait_for_RED_DOWN_button_press():
     # TODO: 4.  Implement and test this function.
     #           Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    RC = ev3.RemoteControl(2)
+    while RC.red_down == True:
+        print(RC.red_down)
 
 
 def run_test_make_sounds():
