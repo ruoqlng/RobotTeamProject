66185487c729fbc61bf362eb6691b7f0bb210a15
zouz-sean <zouz@rose-hulman.edu>
2018-05-12 16:56:35 -0400

    robot

--- sandbox/Sean/mqtt/m5_pc_remote_drive.py
+++ sandbox/Sean/mqtt/m5_pc_remote_drive.py
@@ -35,6 +35,8 @@ def main():
     # TODO: 2. Setup an mqtt_client.  Notice that since you don't need to receive any messages you do NOT need to have
     # a MyDelegate class.  Simply construct the MqttClient with no parameter in the constructor (easy).
     mqtt_client = None  # Delete this line, it was added temporarily so that the code we gave you had no errors.
+    def MyDelegate:
+
 
     root = tkinter.Tk()
     root.title("MQTT Remote")
