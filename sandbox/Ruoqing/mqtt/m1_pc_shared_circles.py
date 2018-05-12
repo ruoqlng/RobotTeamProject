40d0ab272ae963099c67794d815b747e25b40540
Ruoqing <ouyangr@rose-hulman.edu>
2018-05-12 15:12:12 -0400

    １

--- sandbox/Ruoqing/mqtt/m1_pc_shared_circles.py
+++ sandbox/Ruoqing/mqtt/m1_pc_shared_circles.py
@@ -114,7 +114,7 @@ def left_mouse_click(event, mqtt_client):
     # Additionally you will receive your own message and draw a circle in your color too.
     mqtt_client.send_message("on_circle_draw", [my_color, event.x, event.y])
 
-    # TODO: 8. Help get everyone on your team running this program at the same time.
+    # DONE: 8. Help get everyone on your team running this program at the same time.
     # You should be able to see circles on your computer from everyone else on your team.
     # Try to draw the first letter of your name in circles. :)
 
