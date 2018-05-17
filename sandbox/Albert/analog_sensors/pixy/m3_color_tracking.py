81992d06b615bd04afe7a4c68e72ae7e2bc27f2e
GodMasterLee <lib1@rose-hulman.edu>
2018-05-16 20:17:13 -0400

    Albert :)

--- sandbox/Albert/analog_sensors/pixy/m3_color_tracking.py
+++ sandbox/Albert/analog_sensors/pixy/m3_color_tracking.py
@@ -29,15 +29,24 @@ def main():
 
     while not robot.touch_sensor.is_pressed:
 
-        # TODO: 2. Read the Pixy values for x and y
+        # Done: 2. Read the Pixy values for x and y
         # Print the values for x and y
-
+        print("value1: X", robot.pixy.value(1))
+        print("value2: Y", robot.pixy.value(2))
         # TODO: 3. Use the x value to turn the robot
         #   If the Pixy x value is less than 150 turn left (-turn_speed, turn_speed)
         #   If the Pixy x value is greater than 170 turn right (turn_speed, -turn_speed)
         #   If the Pixy x value is between 150 and 170 stop the robot
         # Continuously track the color until the touch sensor is pressed to end the program.
-
+        if robot.pixy.value(1)<150:
+            robot.left(-turn_speed, turn_speed)
+        if robot.pixy.value(1)>170:
+            robot.right(turn_speed, -turn_speed)
+        if robot.pixy.value(1)>150:
+            if robot.pixy.value(1)<170:
+             robot.stop()
+        if robot.pixy.value(1) == 0:
+            robot.stop()
 
 
         time.sleep(0.25)
